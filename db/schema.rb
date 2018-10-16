4b4290b2135541a4ff315b8abe29b59df7de5374
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-16 21:08:50 +0200

    models lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_132208) do
+ActiveRecord::Schema.define(version: 2018_10_15_202956) do
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
