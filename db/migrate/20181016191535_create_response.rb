8d4a793e7ac70d9c71438766488d9260330c2c28
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-16 21:15:56 +0200

    response - added

---
+++ db/migrate/20181016191535_create_response.rb
@@ -0,0 +1,7 @@
+class CreateResponse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :responses do |t|
+      t.text :response
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_191423) do
+ActiveRecord::Schema.define(version: 2018_10_16_191535) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.text "response"
   end
 
   create_table "tests", force: :cascade do |t|
