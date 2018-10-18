213042bb9c3c39b3e5078dd8e732de5d02dc784d
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-18 11:14:46 +0200

    test class method for sorting

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+
+  belongs_to :category
+
+  def self.sorted_by_category(title)
+    instances_by_category = Test.all.select { |instance| instance.category.title == title }
+    instances_by_category.sort_by { |instance| instance.title.downcase }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_193636) do
+ActiveRecord::Schema.define(version: 2018_10_18_083857) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "responses", force: :cascade do |t|
     t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "correct", default: false
   end
 
@@ -30,12 +36,15 @@ ActiveRecord::Schema.define(version: 2018_10_16_193636) do
     t.integer "level", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "correct", default: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
