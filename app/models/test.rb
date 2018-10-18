0a54943b0f55485c869acc422213ff127662592a
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-18 11:29:29 +0200

    rewritten with active record ql

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,9 @@ class Test < ApplicationRecord
 
   belongs_to :category
 
+  scope :by_category, -> (title) { joins(:category).where(categories: {title: title }) }
+
   def self.sorted_by_category(title)
-    instances_by_category = Test.all.select { |instance| instance.category.title == title }
-    instances_by_category.sort_by { |instance| instance.title.downcase }
+    by_category(title)
   end
 end
