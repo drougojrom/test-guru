7d967ea967ff0b9385728b1169a7dec4577dbfd8
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-18 21:21:02 +0200

    fixed with active record query interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
   def completed_tests_for(level)
-    tests.select { |test| test.level == level }
+    tests.where(level: level)
   end
 end
