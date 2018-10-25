9bc25b1fa8b8a0d13e30fb1d89cb95780f34a438
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-25 16:51:22 +0200

    answer validation - updated

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,16 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :text, presence: true
+  validate :validate_answers_count
 
   scope :correct, -> { where(correct: true) }
+
+
+  private
+
+  def validate_answers_count
+    if question.answers.size >= 4
+      errors.add(:question, 'can not have more than 4 answers')
+    end
+  end
 end
