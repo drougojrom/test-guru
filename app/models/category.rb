4143cfeebad95aee318c33c62f4a7896f86bd910
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-10-25 16:51:51 +0200

    default scope for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :tests
 
-  scope :sorted, -> { order(title: :asc) }
+  default_scope { order(title: :asc) }
 
   validate :title, presence: true,
     uniqueness: true
