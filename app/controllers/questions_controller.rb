0f1437533365d13746f2ee01c32f82651fbdc5cf
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-28 22:46:41 +0100

    rescue - added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,26 @@
 class QuestionsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound,
+    with: rescue_question_not_found
+
   def index
+  end
+
+  def new
+  end
+
+  def show
   end
 
   def create
   end
 
   def destroy
+  end
+
+private
+
+  def rescue_question_not_found
+    render plain: 'Question not found'
   end
 end
