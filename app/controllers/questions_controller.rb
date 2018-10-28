20445237dd4c88369acd857f6b4533cf0c644e8e
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-28 22:54:05 +0100

    actions - added, view - init

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,8 @@
 class QuestionsController < ApplicationController
 
+  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:index, :new, :create]
+
   rescue_from ActiveRecord::RecordNotFound,
     with: rescue_question_not_found
 
@@ -13,12 +16,28 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @test.questions.create(question_params)
+    redirect_to test_questions_url
   end
 
   def destroy
+    @question.destroy
+    render plain: 'The question was removed from db'
   end
 
 private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
   def rescue_question_not_found
     render plain: 'Question not found'
