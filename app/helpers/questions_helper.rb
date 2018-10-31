e7ae63a0414a91a141082c37ebc6831aa77fd792
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-10-31 23:50:55 +0100

    create new question - added, footer - added

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,11 @@
 module QuestionsHelper
+  def question_header(question)
+    test = question.test.title
+
+    if question.new_record?
+      "Create New #{test} Question"
+    else
+      "Edit #{test} Question"
+    end
+  end
 end
