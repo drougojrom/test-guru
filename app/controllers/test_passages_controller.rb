3c3be42e2409c532e4ff092f061507f4a45414ce
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-06 17:05:28 +0100

    correct question - added

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,4 +1,6 @@
 class TestPassagesController < ApplicationController
+
+  before_action :set_test_passage, only: %i[show update result]
   def show
   end
 
@@ -6,5 +8,13 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+    @test_passage.accept!(params[:answer_ids])
+    render :show
+  end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 end
