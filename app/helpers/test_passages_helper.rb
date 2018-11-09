bc600fd0629340d70498019bd026f3a124f3742f
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-09 10:22:30 +0100

    fixes after review

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,5 +1,2 @@
 module TestPassagesHelper
-  def result_success?(test_passage)
-    test_passage.result >= 85
-  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,12 +4,12 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  after_validation :after_validation_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-    self.current_question = next_question
     save!
   end
   
@@ -29,7 +29,15 @@ class TestPassage < ApplicationRecord
     test.questions.count
   end
 
+  def success?
+    result >= 85 ? "pos" : "neg"
+  end
+
   private
+
+  def after_validation_set_next_question
+    self.current_question = next_question
+  end
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
