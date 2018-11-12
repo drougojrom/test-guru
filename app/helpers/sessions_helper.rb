d1a2f22b79a4370c9e0d74edd3c0b32161210369
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-12 11:00:03 +0100

    alert helper, session id after create

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def flash_text(key, message)
+    flash[key] ||= message unless message.blank?
+  end
+
+  def flash_text_now(key, message)
+    flash.now[key] ||= message unless message.blank?
+  end
 end
