0bbac42ea58794e7d2084d42cd0839da939e561e
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-11-12 08:54:36 +0100

    sessions update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      return redirect_to login_path, alert: 'Are you a guru? Try to log in again'
     end
+    cookies[:email] = current_user.email
   end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def logged_id?
+  def logged_in?
     current_user.present?
   end
 end
