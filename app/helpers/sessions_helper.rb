f806a149af52ba5b1563110646d82921f7f0feb8
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-13 10:38:25 +0100

    alert fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,9 @@
 module SessionsHelper
-  def flash_text(key, message)
-    flash[key] ||= message unless message.blank?
-  end
-
-  def flash_text_now(key, message)
-    flash.now[key] ||= message unless message.blank?
+  def flash_message
+    flashes = flash.map do |key, message|
+      render partial: 'shared/alert', locals: { text: message, css_class: key }
+    end
+    safe_join flashes
   end
 
   def redirect_back_or(default)
