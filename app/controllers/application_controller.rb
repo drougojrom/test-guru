68b55058529250aa203c163e2265506996ddafe2
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-11-22 20:03:43 +0100

    default locale fix, footer/header fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { lang: I18n.locale }
+    if I18n.locale != I18n.locale
+      { lang: I18n.locale }
+    else
+      {}
+    end
   end
 
   private
