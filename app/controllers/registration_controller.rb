1a35674128e96a1621cb40ee4aaf1f018e6a6921
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-26 10:15:49 +0100

    links fixed, alerts update

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,7 +7,7 @@ private
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)    
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 end
 
