d3ee8375b2d5c13d3e7bd2841f2e48eaf7bda3d6
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-11-28 13:36:05 +0100

    localization - added

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -16,7 +16,7 @@ class GistQuestionService
 
   def gist_params
     {
-      description: "A question about #{@test.title} from TestGuru",
+      description: "#{I18n.t('test_passages.gist.question_title')} #{@test.title}",
       files: {
         'test-guru-question.txt' => {
           content: gist_content
