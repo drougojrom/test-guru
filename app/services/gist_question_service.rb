58e541dada825ab7a44405f24d292b1deffaec0f
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-11-29 23:29:07 +0100

    github client - removed

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -1,14 +1,22 @@
 class GistQuestionService
+
+  ROOT_ENDPOINT = 'https://api.github.com'
+  ACCESS_TOKEN = ENV['ACCESS_TOKEN']
+
   attr_accessor :result
 
   def initialize(question, client: nil)
     @question = question
     @test = @question.test
-    @client = client || GitHubClient.new
+    @client ||= Octokit::Client.new(:access_token => "#{ACCESS_TOKEN}")
   end
 
   def call
-    @result = @client.create_gist(gist_params)
+    @result = create_gist(gist_params)
+  end
+
+  def create_gist(params)
+    @client.create_gist(params)
   end
 
   def success?
