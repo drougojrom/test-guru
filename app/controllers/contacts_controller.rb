b0592890bbc0eced7b5e64f1a5c9d77eec809bf4
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-12-13 13:10:12 +0100

    redirect fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
     @contact_model = Contact.new(contact_params)
     if @contact_model.valid?
       ContactsMailer.submited_form(@contact_model).deliver_now
-      redirect_to contactus_path, notice: "Thank you for your feedback! We'll contact you soon!"
+      redirect_to tests_path, notice: "Thank you for your feedback! We'll contact you soon!"
     else
       render :index
     end
