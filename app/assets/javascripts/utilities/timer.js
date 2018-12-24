e43baba5428a58c58242dfb44df80eceaa3a7f93
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-12-24 23:13:58 +0100

    timer - added

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,7 +1,31 @@
 document.addEventListener('turbolinks:load', function() {
   var countdownLabel = document.querySelector('.countdownLabel')
   if (countdownLabel) {
-    var current = countdownLabel.dataset.current
-    $('.countdownLabel').countdown({until: current})
+    var seconds = countdownLabel.dataset.seconds;
+    if (seconds) {
+      createTimer();
+    } else {
+      return
+    }
   }
-};
+
+  function createTimer() {
+    if (seconds < 1) {
+      countdownLabel.textContent = "Time is out";
+      return;
+    }
+    seconds--;
+    countdownLabel.textContent = secondsToTime(seconds)    
+    if (seconds == 0) {
+      alert("Time is out! You'll be redirected to tests");
+      window.location.href = '/tests';
+    }
+    setTimeout(createTimer, 1000)
+  }
+
+  function secondsToTime(seconds) {
+    var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+    return "You have left: " + numminutes + " minutes, " + numseconds + " seconds";
+  }
+});
